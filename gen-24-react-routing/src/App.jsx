59f04e73ff43ed4bfb89f64f4a8832ff2a530dee
kattriananda <kattriananda@gmail.com>
2024-06-01 17:39:14 +0700

    Update Routing

--- gen-24-react-routing/src/App.jsx
+++ gen-24-react-routing/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
   //<Router>
       <Routes>
-     
+        <Route path="/" element={<Home/>}/>
         <Route path="/home" element={<Home/>}/>
         <Route path="/detail/:id" element={<Detail/>}/>
       </Routes>
